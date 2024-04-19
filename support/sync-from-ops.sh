7d71a4bc33f34696520c86d24b7428abd63a5c77
Sean Kelly <kelly@seankelly.biz>
2024-04-19 15:37:37 -0500

    Sync NIST help DB from ops, attempt 1

---
+++ support/sync-from-ops.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+#
+# Sync NIST Help Portal DB
+# ========================
+#
+# Synchronize the NIST Help portal DB from ops
+
+PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
+export PATH
+BACKUP_DIR=${BACKUP_DIR:-/home/ddsaops/backups/nisthelp}
+MEDIA_DIR=${MEDIA_DIR:-/home/ddsaops/nist-help/media}
+WORKSPACE=${WORKSPACE:-${PWD:-`pwd`}}
+source="ddsa-labcas.jpl.nasa.gov"
+
+# Do it
+# -----
+
+[ -d ${WORKSPACE}/media ] || mkdir ${WORKSPACE}/media
+rm --force latest.sql.bz2
+scp $source:$BACKUP_DIR/database/latest.sql.bz2 .
+rsync --checksum --no-motd --recursive --delete --progress $source/$MEDIA_DIR ${WORKSPACE}
+
+# Done!
+# -----
+
+exit 0
